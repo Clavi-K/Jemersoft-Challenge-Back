596917fb9a6a70a9ebd4a30732e08718ac1cd664
Luciano Schmidt <luciano43schmidt@gmail.com>
2022-12-02 09:43:28 -0300

    (feature) get all pokemons endpoint

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -7,6 +7,21 @@ const service = require("../services/api.service")
 /* ----- MODULE EXPORT ----- */
 
 module.exports = {
+    
+    getAll: async (req, res) => {
+
+        try {
+
+            const result = await service.getAll()
+            console.log(result)
+            return res.status(200).send(result)
+
+        } catch (e) {
+            console.log(e)
+            return res.status(500).send("Error: Could not return all pokemons")
+        }
+
+    }
 
 }
 
