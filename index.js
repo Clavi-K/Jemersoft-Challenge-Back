655daebc0ba6fd4205205f0aa85d8bb614a782c1
Luciano Schmidt <luciano43schmidt@gmail.com>
2022-12-03 09:42:51 -0300

    (update) cors update

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(
     cors({
-        origin: ["http://localhost:3000"],
+        origin: ["http://localhost:3000", "https://jemersoftpokemon.netlify.app"],
         credentials: true,
         methods: "GET"
     })
